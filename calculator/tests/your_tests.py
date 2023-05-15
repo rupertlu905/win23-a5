1cd8c9feb1084da4bb5036eea38935013e7eca10
rupertlu <rupertlu@stanford.edu>
2023-05-15 16:22:36 -0700

    Added my own CI tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,13 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
-###
+# Checks that the program outputs "1" for an input of "5 - 4"
+assert run("5 - 4").output == "1"
+# Checks that the program outputs "2" for an input of "8 / 4"
+assert run("8 / 4").output == "2"
+# Checks that the program exits successfully (no error) for input "5 - 4"
+assert run("5 - 4").exit_status == 0
+# Checks that the program fails (correctly errors) for input "4 & 8"
+assert run("4 & 8").exit_status != 0
 
 print("All tests passed!")
